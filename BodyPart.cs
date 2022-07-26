f77a72e72ad151ff3a853d7aa102264246fbe740
Monica leonte <monica_leonte@outlook.com>
2022-07-26 17:05:09 -0400

    Added missing file

---
+++ BodyPart.cs
@@ -0,0 +1,19 @@
+﻿
+    public enum BodyPart
+    {
+        Head,
+        Helmet,
+        Eyebrows,
+        FacialHair,
+        Hair,
+        Torso,
+        UpperArmLeft,
+        UpperArmRight,
+        LowerArmLeft,
+        LowerArmRight,
+        HandLeft,
+        HandRight,
+        LegLeft,
+        LegRight,
+        Hips
+    }
